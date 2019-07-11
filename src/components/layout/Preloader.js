ab66f2b24e7c7f2ac93391c451d52945070ef9c9
thanhthedev <huythanhdao@gmail.com>
2019-07-11 16:29:09 +1000

    added moment

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <div className="progess blue lighten-4">
+      <div className="indeterminate blue" />
+    </div>
+  );
+};
+
+export default Preloader;
